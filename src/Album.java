cff225ba64a0c81e65ad7c05a860552d6f8c9195
Japhet Ndemera <jndemera2@student.gsu.edu>
2017-10-26 15:09:07 -0400

    Create Album.java

---
+++ src/Album.java
@@ -0,0 +1,76 @@
+public final class Album implements Comparable<Album> {
+
+    private int id;
+    private String title;
+    private String artist;
+    private Image artwork;
+    private ArrayList<Song> songs;
+    private SimpleObjectProperty<Image> artworkProperty;
+
+    /**
+     * Constructor for the Album class. 
+     * Creates an album object and obtains the album artwork.
+     *
+     * @param id
+     * @param title
+     * @param artist
+     * @param songs
+     */
+    public Album(int id, String title, String artist, ArrayList<Song> songs) {
+        this.id = id;
+        this.title = title;
+        this.artist = artist;
+        this.songs = songs;
+        this.artworkProperty = new SimpleObjectProperty<>(getArtwork());
+    }
+
+    /**
+     * Gets album ID.
+     *
+     * @return album ID
+     */
+    public int getId() {
+        return this.id;
+    }
+
+    /**
+     * Gets album title
+     *
+     * @return album title
+     */
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getArtist() {
+        return this.artist;
+    }
+
+    public ArrayList<Song> getSongs() {
+        return new ArrayList<>(this.songs);
+    }
+
+    public ObjectProperty<Image> artworkProperty() {
+        return this.artworkProperty;
+    }
+
+    public Image getArtwork() {
+        
+    }
+
+    public void downloadArtwork() {
+        
+    }
+
+    @Override
+    public int compareTo(Album other) {
+        String first = removeArticle(this.title);
+        String second = removeArticle(other.title);
+
+        return first.compareTo(second);
+    }
+
+    private String removeArticle(String title) {
+        //removes "a", "an" and "the" from the name of the album
+    }
+}
