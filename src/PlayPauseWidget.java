7018eba8711ac99d195011f9349d47a41951c217
Japhet Ndemera <jndemera2@student.gsu.edu>
2017-10-26 22:09:14 -0400

    Update PlayPauseWidget.java

--- src/PlayPauseWidget.java
+++ src/PlayPauseWidget.java
@@ -1,6 +1,9 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package musicplayer;
-
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -9,20 +12,14 @@ import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import com.jfoenix.controls.*;
-import java.awt.Image;
 import javafx.geometry.*;
-import javafx.scene.image.ImageView;
 import musicplayer.Song;
 /**
  *
  * @author jndemera2
  */
 public class PlayPauseWidget extends GridPane{
-    JFXButton playLists = new JFXButton("PlayLists");
-    JFXButton artists = new JFXButton("Artists");
-    JFXButton albums = new JFXButton("Albums");
-    JFXButton songs = new JFXButton("Songs");
-    JFXButton genres = new JFXButton("Genres");
+    
         
     JFXButton play = new JFXButton("Play");
     JFXButton pause = new JFXButton("Pause");
@@ -34,7 +31,7 @@ public class PlayPauseWidget extends GridPane{
     JFXSlider scrubThrough = new JFXSlider();
     
     //JFXLabel currentSongDisplay = new JFXLabel("Current song");
-    public PlayPauseWidget(){
+    public  PlayPauseWidget(){
         
         setPadding(new Insets (10,10,10,10));
         setVgap(10);
@@ -43,77 +40,62 @@ public class PlayPauseWidget extends GridPane{
         Region region = new Region();
         GridPane.setHgrow(region, Priority.ALWAYS);
         GridPane.setVgrow(region, Priority.ALWAYS);
+        
         Region r = new Region();
         GridPane.setHgrow(r, Priority.ALWAYS);
         GridPane.setVgrow(r, Priority.ALWAYS);
+        
+        
+        add(last,0,2);
+        add(play,1,2);
+        add(pause,2,2);
+        add(next,3,2);
+        add(region,4,0);
+        add(currentSongDisplay, 5,2);
+        add(scrubThrough,5,1);
+        add(r,6,0);
+        add(volume,7,2);
+        add(volumeSlider,8,2);
         
         scrubThrough.setMaxWidth(Double.MAX_VALUE);
         setHgrow(scrubThrough, Priority.ALWAYS);
         currentSongDisplay.setPrefWidth(250);
         currentSongDisplay.setPrefHeight(50);
-        
         currentSongDisplay.setDisable(true);
-        currentSongDisplay.setText("Song: I'm Here \nArtist: Russ \nAlbum: There's Really a Wolf ");
-        //last.setStyle("-fx-background-image: url('C:/Users/ndeme/Documents/NetBeansProjects/MusicPlayer/src/musicplayer/last.png')");
+        currentSongDisplay.appendText("Song: I'm here \nArtist: Russ \nAlbum: There's Really A Wolf");
         
-        //Image imageOk = new Image(getClass().getResourceAsStream("ok.png"));
-        last.setBackground(Background.EMPTY);
-
-        //last.setStyle("-fx-graphic: 22 arial; -fx-base: #b6e7c9;");
-        //Image imageOk = new Image(getClass().getResourceAsStream("ok.png"));
-        add(playLists, 0, 0);
-        add(artists, 0, 1);
-        add(albums, 0, 2);
-        add(songs, 0, 3);
-        add(genres, 0, 4);
-        
-        add(last,0,7);
-        add(play,1,7);
-        add(pause,2,7);
-        add(next,3,7);
-        add(region,4,5);
-        add(currentSongDisplay, 5,7);
-        add(scrubThrough,5,6);
-        add(r,6,5);
-        add(volume,7,7);
-        add(volumeSlider,8,7);
-        
-
         
         last.setOnAction(new EventHandler<ActionEvent>() {
+            
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                //System.out.println("Hello World!");
                 Song d = new Song();
                 d.last();
             }
         });
                 
         play.setOnAction(new EventHandler<ActionEvent>() {
+            
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-                //Song d = new Song();
-                //d.playMp3();
-            }
-        });
-        
-        pause.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                //System.out.println("Hello World!");
                 //Song d = new Song();
                 //d.playMp3();
             }
         });
         
         next.setOnAction(new EventHandler<ActionEvent>() {
+            
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                //System.out.println("Hello World!");
                 Song d = new Song();
                 d.next();
             }
-        }); 
+        });
+
     }
+    
+
 }
