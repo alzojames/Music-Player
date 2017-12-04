e6ea7570628832c3e8fb9f9a33373f6b0e5d34d3
Zaxer10 <32685031+Zaxer10@users.noreply.github.com>
2017-12-04 13:12:22 -0500

    Add files via upload

--- src/Artist.java
+++ src/Artist.java
@@ -1,215 +1,170 @@
-/*
- * This class dynamically adds ablum art when a new album in a grid
- * 
- */
-
-/**
- *
- * @author Japhet
- */
-
 package musicplayer;
 
-import com.google.common.io.Resources;
-import java.awt.Color;
-import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.image.BufferedImage;
-import java.awt.image.ImageObserver;
-import java.awt.image.ImageProducer;
-import java.io.File;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.HashSet;
+
+import java.util.*;
+
 import javafx.beans.property.ObjectProperty;
+
 import javafx.beans.property.SimpleObjectProperty;
-import javax.imageio.ImageIO;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamReader;
+
 import musicplayer.Album;
 
 public final class Artist implements Comparable<Artist> {
+	
 
     private String name;
-    HashSet<Album> albums = new HashSet<Album>();
+    
+    static HashSet<Album> albums = new HashSet<Album>();
+    
     private Image artistImage;
+    
     private SimpleObjectProperty<Image> artistImageProperty;
+    
     ArtistWidget artistWidget;
-    ArtistArtWork artWork;
-    private String title;
 
     /**
+     * 
      * Constructor for the Artist class.
+     * 
      * Creates an artist object and obtains the artist artwork.
      *
      * @param name Artist name
+     * 
+     * @param albums List of artist albums
+     * 
      */
-    public Artist(String name)
-    {
+    
+    public Artist(String name) {
+    	
 
-        if(name == null)
-        {
+        if(name == null){
+        	
             this.name = "Unkown";
+            
         }else{
+        	
             this.name = name;
+            
         }
         
         artistWidget = new ArtistWidget();
-        //this.artistImageProperty = new SimpleObjectProperty<>(getArtistImage());
-
+        
+        this.artistImageProperty = new SimpleObjectProperty<>(getArtistImage());
+        
     }
 
     /**
+     * 
      * Gets the artist title.
+     * 
      * @return artist title
+     * 
      */
-    public String getName()
-    {
+    
+    public String getName() {
+    	
         return this.name;
+        
     }
 
     /**
+     * 
      * Gets array list of artist albums
+     * 
      * @return artist albums
+     * 
      */
-//    public ArrayList<Album> getAlbums()
-//    {
-//        return new ArrayList<Album>(this.albums);
-//    }
+    
+    //public ArrayList<Album> getAlbums() {
+    
+      //  return new ArrayList<>(this.albums);
+    
+    //}
+    
 
-    public ObjectProperty<Image> artistImageProperty()
-    {
+    public ObjectProperty<Image> artistImageProperty() {
+    	
         return this.artistImageProperty;
+        
     }
 
     /**
+     * 
      * Gets images for artists
+     * 
      * @return artist image
+     * 
      */
-    public Image getArtistImage()
-    {
-         if (artistImage == null) {
-            try {
-                File file = new File("/img/" + this.name + ".jpg");
-                if (artistImage.equals(file)) {
-                    return artistImage;
-                    
-                }
-            } catch (Exception ex) {
-                File file = new File("/img/" + this.name + ".jpg");
-                file.delete();
-            }
-            return artistImage;
-        }
-         return artistImage;
-    }
-    
-    
-
-    public void downloadArtistImage()
-    {
-        try {
-            File file = new File("/img/" + this.title + ".jpg");
-            file.mkdirs();
-            XMLInputFactory factory = XMLInputFactory.newInstance();
-            URL xmlData = new URL("method=artist.getinfo"
-                    + "&artist=" + URLEncoder.encode(this.title, "UTF-8")
-                    + "&api_key=");
-            XMLStreamReader reader = factory.createXMLStreamReader(xmlData.openStream(), "UTF-8");
-            boolean imageFound = false;
-
-            while (reader.hasNext() && !imageFound) {
-                reader.next();
-
-                if (reader.isStartElement()
-                        && reader.getName().getLocalPart().equals("image")
-                        && reader.getAttributeValue(0).equals("extralarge")) {
-
-                    reader.next();
-
-                    if (reader.hasText()) {
-                        BufferedImage bufferedImage = ImageIO.read(new URL(reader.getText()));
-                        BufferedImage newBufferedImage = new BufferedImage(bufferedImage.getWidth(),
-                                bufferedImage.getHeight(), BufferedImage.TYPE_INT_RGB);
-                        newBufferedImage.createGraphics().drawImage(bufferedImage, 0, 0, Color.WHITE, null);
-                        ImageIO.write(newBufferedImage, "jpg", file);
-                        imageFound = true;
-                    }
-                }
-            }
-
-            
-            if (artistImage.equals(file)) {
-                file.delete();
-                
-            }
-            this.artistImageProperty.setValue(artistImage);
-
-        } catch (Exception ex) {
-            File file = new File("/img/" + this.name + ".jpg");
-            file.delete();
-        }
+    public Image getArtistImage() {
+    	
+        return null;
+        
+        //gets Image of the artist for the gui
+        
     }
 
-    public String removeArticle(String title)
-    {
+    public void downloadArtistImage() {
+    	
+        
+    }
+    
+    /**
+     * 
+     * Removes articles( "a", "an" and "the") from album titles
+     * 
+     * @return null
+     * 
+     */
+    
+    private String removeArticle(String title) {
+    	
+        return null;
         
         //removes "a", "an" and "the" from the name of the artist
-        String arr[];
-        arr = title.split(" ", 2);
-
-        if (arr.length < 2) {
-            return title;
-        } else {
-
-            String firstWord = arr[0];
-            String theRest = arr[1];
-
-            switch (firstWord) {
-                case "A":
-                case "An":
-                case "The":
-                    
-                    return theRest;
-                default:
-                    return title;
-            }
-        }
-    }
-    
-    public void setArt()
-    {
-        artWork = new ArtistArtWork(this.name);
-    }
-    
-    public ArtistArtWork getArt(){
-        return artWork; 
+        
     }
 
     @Override
-    public int compareTo(Artist other)
-    {
-        String first = removeArticle(this.title);
-        String second = removeArticle(other.title);
-
-        return first.compareTo(second);
+    
+    public int compareTo(Artist o) {
+    	
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
     
-    public void addSong(Song song)
-    {
+    /**
+     * Adds the album to the artist class
+     * 
+     */
+    
+    public static void addAlbums(Album album){
+    	
+        albums.add(album); 
+        
+        
+    }
+    
+    /**
+     * Adds the songs to the artist class
+     * 
+     */
+    
+    public void addSong(Song song){
+    	
         artistWidget.addSong(song);
+        
     } 
     
-    public ArtistWidget getArtistWidget()
-    {
+    /**
+     * Adds the artist name to the artist class
+     * 
+     */
+    
+    public ArtistWidget getArtistWidget(){
+    	
         return artistWidget;
+        
     }
-    
-    public HashSet<Album> getAlbums(){
-        return this.albums;
-    }
-    
-
-    
     
 }
