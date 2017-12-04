50baf7277767a24dd7bd543727388bc4e89feb22
Zaxer10 <32685031+Zaxer10@users.noreply.github.com>
2017-12-04 13:14:46 -0500

    Add files via upload

--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -1,5 +1,6 @@
 /**
  * This class will display all the assests of the program
+
  * and will handle operation regurding the play, pause, next song
  * prevous song etc.
  *
@@ -11,84 +12,197 @@
  */
 package musicplayer;
 
+
 import javafx.application.Application;
+
+
 import javafx.event.ActionEvent;
+
+
 import javafx.event.EventHandler;
+
+
 import javafx.scene.Scene;
+
+
 import javafx.scene.layout.*;
+
+
 import javafx.stage.Stage;
+
+
 import com.jfoenix.controls.JFXButton;
+
+
 import com.jfoenix.controls.JFXTabPane;
+
+
 import java.awt.Color;
+
+
 import java.io.File;
+
+
 import static java.lang.Math.log;
+
+
 import java.lang.reflect.InvocationTargetException;
+
+
 import java.util.ArrayList;
+
+
 import java.util.Collections;
+
+
 import java.util.Comparator;
+
+
 import java.util.HashMap;
+
+
 import java.util.Iterator;
+
+
 import java.util.List;
+
+
 import java.util.concurrent.TimeUnit;
+
+
 import java.util.logging.Level;
+
+
 import java.util.logging.Logger;
+
+
 import javafx.application.Platform;
+
+
 import javafx.beans.InvalidationListener;
+
+
 import javafx.beans.Observable;
+
+
 import javafx.beans.value.ChangeListener;
+
+
 import javafx.beans.value.ObservableValue;
+
+
 import javafx.collections.ObservableList;
+
+
 import javafx.geometry.Pos;
+
+
+
 import javafx.scene.Group;
+
+
 import javafx.scene.control.Alert;
+
+
 import javafx.scene.control.Button;
+
+
 import javafx.scene.control.Label;
+
+
 import javafx.scene.control.ScrollPane;
+
+
 import javafx.scene.control.Tab;
+
+
 import javafx.scene.image.Image;
+
+
 import javafx.scene.image.ImageView;
+
+
 import static javafx.scene.layout.GridPane.setHgrow;
+
+
 import javafx.scene.media.Media;
+
+
 import javafx.scene.media.MediaPlayer;
+
+
 import javafx.scene.media.MediaView;
+
+
 import javafx.stage.FileChooser;
+
+
 import javafx.util.Duration;
+
+
 import static musicplayer.MusicPlayer.mediaPlayer;
 
 
+/**
+ * This class sets up the GUI for the Music Player. Assigns the tabs for PLaylists, Artists, Albums, Songs, and Genres. 
+ * 
+ * 
+ */
 public class MusicPlayer extends Application {
         
     private static final double minChange = 0.5;
+    
     static MediaPlayer mediaPlayer;
+    
     static Media media;
+    
     private Label time;
+    
     static MediaView mediaView;
+    
     static String path;
+    
     Duration duration;
+    
     Button fullScreenButton;
   
     double width;
+    
     double height;
 
     PlayListWidget playList = new PlayListWidget();
+    
     static PlayPauseWidget playPause;
+    
     SongWidget songWidget;
+    
     AlbumArt albumArt;
+    
     ArtistWidgetHolder artistWidgetHolder;
+    
     static AlbumArtWidget albumArtWidget;
     //ArtistWidget artistWidget;
 
     static ScrollPane sp = new ScrollPane();
+    
     static ScrollPane sp2 = new ScrollPane();
+    
     static Tab playListTab = new Tab("Playlists");
+    
     static Tab artistTab = new Tab("Artists");
+    
     static Tab albumTab = new Tab("Albums");
+    
     static Tab songTab = new Tab("Songs");
+    
     static Tab Genre = new Tab("Genres");
         
     static ArrayList<Song> playing;
+    
     static int songIndex;
+    
     ArrayList<Album> albumToMake;
+    
     ArrayList<Artist> artistToMake;
     
     static int numOfImports;
@@ -104,53 +218,86 @@ public class MusicPlayer extends Application {
     {
         
         primaryStage.setTitle("Elysium Music Player");
+        
         Group root = new Group();
+        
         Scene scene = new Scene(root, 1000, 600);
+        
         primaryStage.getIcons().add(new Image("file:logo.jpg"));
         
         JFXTabPane tabPane = new JFXTabPane();
+        
         GridPane grid = new GridPane();
+        
         PlayPauseWidget playMenu = new PlayPauseWidget();
+        
         Tab[] tabs = new Tab[5];
+        
         String tabName[] = {"Playlists","Artists","Albums","Songs","Genres"};
+        
         BorderPane borderPane = new BorderPane();
+        
         playPause = new PlayPauseWidget();
+        
         borderPane.setBottom(playPause);
         
         
+        
         Tab tab;
+        
         playListTab = new Tab("Playlists");
+        
         artistTab = new Tab("Artists");
+        
         albumTab = new Tab("Albums");
+        
         songTab = new Tab("Songs");
+        
         Genre = new Tab("Genres");
         
         sp.setFitToWidth(true);
+        
         sp.setContent(albumArtWidget = new AlbumArtWidget());
         
         sp2.setFitToWidth(true);
+        
         sp2.setContent(artistWidgetHolder = new ArtistWidgetHolder());
         
         //create new tabs and put their widgets in them 
+        
         for (int i = 0; i < 5; i++)
+        
         {
             
             tab = new Tab();
+            
             tab.setText(tabName[i]);
+            
             HBox hbox = new HBox();
             
             //Call the Playlist Artist... Widget classes and put the on the TabPane
+            
             switch (i)
+           
             {
                 case 0: playListTab.setContent(new PlayPauseWidget());
+                
                         break;
+                        
                 case 1: artistTab.setContent(sp2);
+                
                         break;
+                        
                 case 2: albumTab.setContent(sp);
+                
                         break;
+                        
                 case 3: songTab.setContent(songWidget = new SongWidget());
+                
                         break;
+                        
                 case 4: Genre.setContent(new PlayPauseWidget());
+                
                         break;
                 
             }
@@ -169,49 +316,75 @@ public class MusicPlayer extends Application {
          *
          */
         playPause.importFiles.setOnAction(
+        		
             (ActionEvent event) -> {
+            	
                 FileChooser fileChooser = new FileChooser();
+                
                 //fileChooser.setMultiSelectionEnabled(true);
+                
                 //Open file explorer that allows user to pick mp3 files
+                
                 FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("mp3 files (*.mp4)", "*.mp3");
+                
                 fileChooser.getExtensionFilters().add(extFilter);
 
 
-                //Put the files in a list and add them to the library
+                
+               //Put the files in a list and add them to the library
+                
                 List<File> list =
+                		
                     fileChooser.showOpenMultipleDialog(primaryStage);
                     
                 numOfImports = 0;
+                
                 albumToMake = new ArrayList<Album>();
+                
                 artistToMake = new ArrayList<Artist>();
                 
                 Thread th = new Thread()
                 {
                     
-                    @Override
-                    public void run()
-                    {
+                 
+                	
+                	@Override
+                
+                	public void run()
+                
+                	
+                	{
                         
                         try
+                
+                        
                         {
-                           importMusic(list);
+                        
+                        	
+                        	importMusic(list);
                            
+                        
                         } catch (Exception e) {
-                            
+                        
+                        	
                         }
 
+                        
                     }
                     
                 };
                                 
                 try
+           
                 {
                     th.start();
+                    
                     //th2.start();
                     
                     TimeUnit.SECONDS.sleep(2);
                     
                     Collections.sort(albumToMake, new Comparator<Album>(){
+                    	
                         public int compare(Album s1, Album s2) {
                             return s1.getTitle().compareToIgnoreCase(s2.getTitle());
                         }
@@ -656,6 +829,7 @@ public class MusicPlayer extends Application {
         
         /*
          * This event handler links the timeSlider to the mediaPlayer
+         * 
          * Allowing the user to select a point in the music they would like to go to
          *
          */
@@ -667,14 +841,18 @@ public class MusicPlayer extends Application {
             {
             
                 mediaPlayer.seek(Duration.millis(playPause.timeSlider.getValue()));
+                
                 System.out.println("Time changing");
+                
             }
             
         });
         
 
         mediaView = new MediaView(mediaPlayer);
+        
         playPause.currentSongDisplay.clear();
+        
         playPause.currentSongDisplay.appendText("Song: " +  song.getTitle() + "\nArtist: " + song.getArtist() + "\nAlbum: " + song.getAlbum());
 
     }
@@ -682,17 +860,24 @@ public class MusicPlayer extends Application {
     /*
      * This method selects the next song to be played 
      * 
+     * 
+     * 
      * @param int index
      *
      */
     public static void selectSong(int index)
+    
     {
 
         try
         {
             mediaPlayer.stop();
+            
+            
             mediaPlayer = null;
+            
             System.gc();
+            
         }catch(NullPointerException e){
             
         }
@@ -701,55 +886,77 @@ public class MusicPlayer extends Application {
         try
         {
             songIndex = index;
+            
             Song song = playing.get(index);
 
+            
             media = new Media(new File(song.getId()).toURI().toString());
+            
             mediaPlayer = new MediaPlayer(media);
+            
             playPause.timeSlider.setDisable(false);
+            
             mediaPlayer.setAutoPlay(true);
+            
             mediaPlayer.setVolume(playPause.volumeSlider.getValue() / 100.0);
+            
             mediaView = new MediaView(mediaPlayer);
             
             String len = song.getDuration();
+            
             Double length = Double.parseDouble(len);
+            
             playPause.timeSlider.setMax(length);
+            
             
             mediaPlayer.setOnEndOfMedia(new Runnable()
             {
 
                 @Override
+                
                 public void run()
+                
                 {
                     selectSong(index+1);
+                
                 }
 
             });
             
             mediaPlayer.currentTimeProperty().addListener(new ChangeListener<Duration>()
+            
             {
 
                 @Override
                 public void changed(ObservableValue<? extends Duration> observable, Duration oldValue, Duration newValue)
+                
                 {
                     playPause.timeSlider.setValue(newValue.toMillis());
+                    
                 }
                 
             });
             
             playPause.timeSlider.setOnMouseClicked(new ListViewHandler()
+            
             {
 
                 @Override
+             
                 public void handle(javafx.scene.input.MouseEvent event)
+                
                 {
                     mediaPlayer.seek(Duration.millis(playPause.timeSlider.getValue()));
+                    
                     System.out.println("Time changing");
                 }
                 
             });
                     
             playPause.currentSongDisplay.clear();
+            
             playPause.currentSongDisplay.appendText("Song: " +  song.getTitle() + "\nArtist: " + song.getArtist() + "\nAlbum: " + song.getAlbum());
+            
 
         }catch (IndexOutOfBoundsException e){
                 
@@ -765,37 +972,53 @@ public class MusicPlayer extends Application {
      *
      */
     public void importMusic(List<File> list)
+    
     {
         
-        System.out.print("New method");        
+        System.out.print("New method");
+        
         Song song;
                 
                 
         if (list != null) 
+        
         {
 
-            for (File file : list)
-            {
+        
+        	for (File file : list)
+        
+        	{
                 numOfImports ++;
 
                 //openFile(file);
+                
                 String fileString = file.toString();
+                
                 //format string so it can be use as URL
+                
                 fileString = fileString.replace("\\", "\\\\");
+                
                 song = new Song(fileString);
+                
 
                 //add the song the library
                 try
                 {
                     Library.addSong(song);
+                    
                 }catch(NullPointerException haha){
+                	
                     System.out.print("Error!!!");
                 }
 
                 //display song on the SongWidget
+                
                 songWidget.addSong(song);
+                
                 //add song to an album
+                
                 addSongToAlbum(song);
+                
 
             }
 
@@ -805,6 +1028,7 @@ public class MusicPlayer extends Application {
         
     /*
      * This method plays selected song and alternates between the play and pause icon
+     * 
      * 
      * @param int index
      *
@@ -816,33 +1040,51 @@ public class MusicPlayer extends Application {
         {
              
             System.out.print("Pause");
+            
             playPause.getChildren().remove(playPause.pause);
+            
             Image lastIcon = new Image(getClass().getResourceAsStream("playIcon.png"));
+            
             playPause.play.setGraphic(new ImageView(lastIcon));
+            
             ImageView pauseView = new ImageView(lastIcon);
+            
             pauseView.setFitWidth(5);
+            
             pauseView.setFitHeight(5);
+            
             playPause.add(playPause.pause,1,2);
 
             MusicPlayer.mediaPlayer.pause();
+            
             System.out.print("Pause!!!!!!");
         }
 
              
         catch(NullPointerException e){
+        	
             System.out.print("Error");
+            
         }
         
         MusicPlayer.selectSong(index);
+        
         Image lastIcon = new Image(getClass().getResourceAsStream("pauseIcon.png"));
+        
         playPause.pause.setGraphic(new ImageView(lastIcon));
+        
         ImageView pauseView = new ImageView(lastIcon);
+        
         pauseView.setFitWidth(5);
+        
         pauseView.setFitHeight(5);
+        
         playPause.add(playPause.pause,1,2);
         
         MusicPlayer.mediaPlayer.play();
                 
     }
+    
+    
 
 }
